b4380fe2afbf853b6e53d33c94fde07d1dda2268
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2022-07-25 18:40:53 -0400

    Delete credentials.py
    Part of gitignore

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# This is just a sample file. You need to
-# edit this file. You need to get these
-# details from your Twitter app settings.
-
-consumer_key = 'oeHWWSnjiVMpXDE2qUvlY7rIZ'
-consumer_secret = 'AnMj4sa6KT6i3xDvG6Kd91YwEVMRB6ZxFSlUOhrBiypj4MAEmY'
-access_token = '1390130402627661827-GUUGMxDQlemqCTzbC6vzOXOnlP7MFk'
-access_token_secret = 'mBKWhu4lYpYzlatEgCEeq8hac43MezWaUDCApwsoTGrRF'
-
